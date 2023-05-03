fa3ab44bd3169537369c36790d559ee63ecc5a7c
Kindi BALDE <kindi.balde@gmail.com>
2023-05-03 16:13:01 +0200

    Update Regression.sh

--- TAB_REGRESSIONS/Regression.sh
+++ TAB_REGRESSIONS/Regression.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python RegressionModels.py  --target_type="BIN" \
+python Regressions.py        --target_type="BIN" \
                              --target_col_name="TARGET" \
-                             --file_path="./data/application_train.csv" \
+                             --file_path="../TAB_DATA/application_train.csv" \
                              --model_filename="model.pkl"
