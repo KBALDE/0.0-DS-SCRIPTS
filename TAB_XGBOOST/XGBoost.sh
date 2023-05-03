fd6e3714afe67cd0bdc54fc9b181e190852d47b5
Kindi BALDE <kindi.balde@gmail.com>
2023-05-03 16:13:37 +0200

    Update XGBoost.sh

--- TAB_XGBOOST/XGBoost.sh
+++ TAB_XGBOOST/XGBoost.sh
@@ -2,5 +2,5 @@
 
 python XGBoost.py   --target_type="BIN" \
                     --target_col_name="TARGET" \
-                    --file_path="./data/application_train.csv" \
+                    --file_path="../TAB_DATA/application_train.csv" \
                     --model_filename="model.pkl"
