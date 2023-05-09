02be81f47e453d847f8a1c6c00d4a23d0c48bae6
kbalde <kindi.balde@gmail.com>
2023-05-09 10:52:59 +0200

    reg file

--- TAB_REGRESSIONS/Regression.sh
+++ TAB_REGRESSIONS/Regression.sh
@@ -4,9 +4,12 @@ DATA_FILE_PATH="./0.0-DS-SCRIPTS/TAB_DATA/application_train.csv"
 
 MODEL_FILE="./0.0-DS-SCRIPTS/TAB_DATA/reg_model.pkl"
 
+TEST_FILE="./0.0-DS-SCRIPTS/TAB_DATA/test_dict.npy"
+
 SCRIPT_ROOT="./0.0-DS-SCRIPTS/TAB_REGRESSIONS"
 
 python $SCRIPT_ROOT/Regression.py   --target_type="BIN" \
                                     --target_col_name="TARGET" \
                                     --file_path=$DATA_FILE_PATH \
-                                    --model_filename=$MODEL_FILE
+                                    --model_filename=$MODEL_FILE \
+                                    --test_filename=$TEST_FILE
